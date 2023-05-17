93fb00f3e07ec42c1f05f3f9c810416c2c4a854c
joshhoneypot <joshuaangugo@gmail.com>
2023-05-17 19:48:11 +0300

    Assignment complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    # name getter
+    def name
+        @name
+    end
 
+    #name setter
+    def name=(name)
+        @name=name
+    end
+
+    #breed getter
+    def breed
+        @breed
+    end
+
+    #breed setter
+    def breed=(breed)
+        @breed=breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    #name getter
+    def name
+        @name
+    end
+
+    #name setter
+    def name=(name)
+        @name=name
+    end
+
+    #job getter
+    def job
+        @job
+    end
+
+    #job setter
+    def job=(job)
+        @job=job
+    end
 
 end
